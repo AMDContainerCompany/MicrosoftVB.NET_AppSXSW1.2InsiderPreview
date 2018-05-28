1656d1089060c4ec5747cc3015b8f086319f2c68
NataliaVodianova <39686420+NataliaVodianova@users.noreply.github.com>
2018-05-28 14:39:15 +0900

    Internet DO ?
    Realistic 42.1 11-12-   CCCCC X ,.,.,,  . .   . .

---
+++ Microsoft VB.NET DB2 Edition Insider Preview/Microsoft VB.NET DB2 Edition Insider Preview/MainPage.xaml.vb
@@ -0,0 +1,20 @@
+﻿Dim log As New EventLog("MyApplication", "Microsoft", "Dispersonator")
+log.WriteEntry("VB.NET-2018", EventLogEntryType.Warning, 124)
+
+Dim log As New EventLog("MyApplication", "Microsoft", "Dispersonator")
+log.WriteEntry("VB.Core-4.17.1", EventLogEntryType.Warning, 124)
+
+Dim log As New EventLog("MyApplication", "Microsoft", "Dispersonator")
+log.WriteEntry("VB.Pennsylvania-5.19.300.e", EventLogEntryType.Warning, 124)
+
+Dim log As New EventLog("MyApplication", "Microsoft", "Dispersonator")
+log.WriteEntry("VB.BusinessIII-15.0", EventLogEntryType.Warning, 124)
+
+Dim log As New EventLog("MyApplication", "Microsoft", "Dispersonator")
+log.WriteEntry("VB.KorgovSystemC-14.300.F-0101", EventLogEntryType.Warning, 124)
+
+Dim log As New EventLog("MyApplication", "Microsoft", "Dispersonator")
+log.WriteEntry("VB.Extra001steller-19.0.0.0.0.82.FVO-VS81-08-01-03-04-05", EventLogEntryType.Warning, 124)
+
+Dim log As New EventLog("MyApplication", "Microsoft", "Dispersonator")
+log.WriteEntry("VB.S-120.10.12.3-FOV-jjjKLOPPS-0124-53-1154-JOKO", EventLogEntryType.Warning, 124)
